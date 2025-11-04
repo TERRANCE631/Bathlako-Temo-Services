a2ff7ef10844fb1555a44fac335bd571f1343ddc
TERRANCE631 <tarrancesiyabonga@gmail.com>
2025-11-04 12:05:55 +0200

    fix: use Tailwind classes for main wrapper and remove unused App.css import

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import './App.css';
 import { Header, Footer } from './Components/index';
 import { Allroutes } from './Routes/Allroutes';
 
 function App() {
-
   return (  
-      <div className="App dark:bg-gray-700">
-        <Header />
-        <Allroutes />
-        <Footer />
-      </div> 
-    );
+    <div className="dark:bg-gray-700 bg-white min-h-screen">
+      <Header />
+      <Allroutes />
+      <Footer />
+    </div> 
+  );
 }
 
 export default App;
